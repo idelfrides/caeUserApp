7ad7975e44432783842b1520139ff84de29a3bfa
idelfrides <idelfridesjorgepapai@gmail.com>
2020-06-27 15:32:12 -0300

    configuring the app

--- neilytaTec1/settings.py
+++ neilytaTec1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['connectafricaeducation.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
